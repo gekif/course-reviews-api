a43c49a0e46c54484bd61c0cd9de77b63074f09a
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-10-02 15:22:25 +0700

    05. Populating and Relationships

--- src/main/java/com/gekif/course/Course.java
+++ src/main/java/com/gekif/course/Course.java
@@ -1,8 +1,13 @@
 package com.gekif.course;
 
 import com.gekif.core.BaseEntity;
+import com.gekif.review.Review;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -10,14 +15,27 @@ public class Course extends BaseEntity {
     private String title;
     private String url;
 
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
     protected Course() {
         super();
+        reviews = new ArrayList<>();
     }
 
     public Course(String title, String url) {
         this();
         this.title = title;
         this.url = url;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void addReview(Review review) {
+        review.setCourse(this);
+        reviews.add(review);
     }
 
     public String getTitle() {
--- src/main/java/com/gekif/review/Review.java
+++ src/main/java/com/gekif/review/Review.java
@@ -1,20 +1,37 @@
 package com.gekif.review;
 
 import com.gekif.core.BaseEntity;
+import com.gekif.course.Course;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 
 @Entity
 public class Review extends BaseEntity {
+    private int rating;
+    private String description;
+
+    @ManyToOne
+    private Course course;
 
     // TODO:csd - We are duplication code here for every single entity. Share it?
     protected Review() {
        super();
     }
 
-    private int rating;
-    private String description;
+    public Review(int rating, String description) {
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 
     public int getRating() {
         return rating;
